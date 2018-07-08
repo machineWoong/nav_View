1996293eda27fe26af7842b7c31ed0f4fd791b12
jiung0802 <jiung0802@naver.com>
2018-07-08 22:59:10 +0900

    Initial commit

---
+++ app/src/main/java/com/example/jeon/nav_view/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.jeon.nav_view;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity   implements NavigationView.OnNavigationItemSelectedListener {
+
+
+
+    public DrawerLayout dl;
+    public ActionBarDrawerToggle mToggle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        dl = (DrawerLayout)findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(MainActivity.this,dl,R.string.open,R.string.close);
+
+
+        dl.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 툴바 활성화
+
+
+        NavigationView mNavigationView = (NavigationView) findViewById(R.id.navi_view);
+
+        if (mNavigationView != null) {
+            mNavigationView.setNavigationItemSelectedListener(this);
+        }
+
+
+    }
+
+
+    //  이걸 넣어줘야지 네이게이션 메뉴가 드로워가 활성화 된다.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+
+        // ...  에있는 항목들.
+        switch (item.getItemId()) {
+            case R.id.home:
+                Toast.makeText(this, "home", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.setting:
+                Toast.makeText(this, "환경설정", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.logout:
+                Toast.makeText(this, "logout", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.nav_menu, menu);
+
+        return true;
+    }
+
+
+    // 네비게이션 아이템 클릭 리스너
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            Toast.makeText(MainActivity.this, "카메라", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_gallery) {
+            Toast.makeText(MainActivity.this, "갤러리", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_slideshow) {
+            Toast.makeText(MainActivity.this, "슬라이드쇼", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_manage) {
+            Toast.makeText(MainActivity.this, "툴", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_share) {
+            Toast.makeText(MainActivity.this, "공유", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_send) {
+            Toast.makeText(MainActivity.this, "보내기", Toast.LENGTH_SHORT).show();
+        }
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer);
+        drawer.closeDrawer(GravityCompat.START);
+
+
+
+        return false;
+    }
+}
